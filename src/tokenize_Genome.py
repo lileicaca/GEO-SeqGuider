3fe43ff9250d02ecd1a4232f4df63a034c5c7758
lileicaca <ll18351806287@gmail.com>
2026-04-09 16:02:06 +0800

    Add files via upload

---
+++ src/tokenize_Genome.py
@@ -0,0 +1,112 @@
+import os
+import random
+import json
+import datetime
+import argparse
+
+# Principle of complementary base pairing
+G_DIC_BASETAB = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+# tokenizer vocabulary
+G_DICT_NUM_DNA_TABLE = {'Appending': 0, 'N': 1, 'Start': 2, 'End': 3, 'A': 4, 'T': 5, 'C': 6,
+                        'G': 7, 'Special': 8, 'label_0': 9, 'label_1': 10}
+G_INT_SEQUENCELENGTH = 2048
+
+
+def getCompleChain(dna: str):
+    comDNA = ''
+    for index in range(len(dna)):
+        if dna[index] in G_DIC_BASETAB:
+            comDNA += G_DIC_BASETAB[dna[index]]
+        else:
+            print(f"Warning:: unknow base {dna[index]}")
+            comDNA += 'N'
+    return comDNA[::-1]
+
+
+def numSequence(in_listSequence: list, in_dictNumTable: dict):
+    numberDNA = []
+    for dna in in_listSequence:
+        currentSeq = []
+        for alphabet in dna:
+            if alphabet not in in_dictNumTable:
+                print(f"Warning :: can't find current vocabulary {alphabet}")
+                currentSeq.append(in_dictNumTable['N'])  # Nucleotide 'N' replace all illegal base
+            else:
+                currentSeq.append(in_dictNumTable[alphabet])
+        if len(dna) == len(currentSeq):
+            currentSeq.insert(0, in_dictNumTable['Start'])
+            currentSeq.append(in_dictNumTable['End'])
+            numberDNA.append(currentSeq)
+        else:
+            print(f"Loss sequence {dna[:100]}")
+    return numberDNA
+
+
+def readAllSequence(in_strCdsFile: str):
+    seq = {}
+    with open(in_strCdsFile) as file:
+        seqName = ''
+        sequence = ''
+        for line in file:
+            if line.startswith('>'):
+                if 0 < len(seqName):
+                    seq[seqName] = sequence.upper()
+                    seq[seqName + 'reverse'] = getCompleChain(sequence.upper())
+                    sequence = ''
+                    seqName = line.strip()[1:].split()[0]
+                else:
+                    sequence += line.strip()
+            if seqName not in seq:
+                seq[seqName] = sequence.upper()
+                seq[seqName + 'reverse'] = getCompleChain(sequence.upper())
+    return seq
+
+
+def splitDNAsequence(in_listNumDNA: list, in_dictNumTable: dict, in_intLength=512):
+    newDNASequence = []
+    for seq in in_listNumDNA:
+        for index in range(len(seq) // in_intLength):
+            newDNASequence.append(seq[in_intLength * index: in_intLength * (index + 1)])
+        if 0 == (len(seq) % in_intLength):
+            continue
+        lastSeq = seq[(-(len(seq) % in_intLength)):]
+        while in_intLength > len(lastSeq):
+            lastSeq.append(in_dictNumTable['Appending'])
+        newDNASequence.append(lastSeq)
+    return newDNASequence
+
+
+def mainNumGenome(genome='data/kt2440.fa', outputDir='input'):
+    DNASequence = readAllSequence(genome)
+    numberDNA = numSequence(list(DNASequence.values()), G_DICT_NUM_DNA_TABLE)
+    numberDNA = splitDNAsequence(numberDNA, G_DICT_NUM_DNA_TABLE, G_INT_SEQUENCELENGTH)
+    random.shuffle(numberDNA)
+    with open(os.path.join(outputDir, f'train_numbase_{G_INT_SEQUENCELENGTH}.jsonl'),
+              'w') as out:
+        for seq in numberDNA[: -25]:
+            out.write(json.dumps(seq) + '\n')
+    with open(os.path.join(outputDir, f'test_numbase_{G_INT_SEQUENCELENGTH}.jsonl'),
+              'w') as out:
+        for seq in numberDNA[-25:]:
+            out.write(json.dumps(seq) + '\n')
+
+
+if __name__ == '__main__':
+    t_dateTime = datetime.datetime.now()
+    print("Begin time : %s" % t_dateTime)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--genome", default='data/kt2440.fa', type=str, required=True,
+        help="The relative or absolute path of the genome file that needs to be tokenized"
+    )
+    parser.add_argument(
+        "--output_dir", default='input', type=str, help="Folder for storing output files."
+    )
+    args = parser.parse_args()
+
+
+    mainNumGenome(args.genome, args.output_dir)
+
+    t_dateTime = datetime.datetime.now()
+    print("End time : %s" % t_dateTime)
---
+++ src/tokenize_crisp.py
@@ -0,0 +1,211 @@
+import os
+from pathlib import Path
+import datetime
+import json
+import random
+import pandas as pd
+import argparse
+
+# tokenizer vocabulary
+G_DICT_NUM_DNA_TABLE = {'Appending': 0, 'N': 1, 'Start': 2, 'End': 3, 'A': 4, 'T': 5, 'C': 6,
+                        'G': 7, 'Special': 8, 'label_0': 9, 'label_1': 10, '+': 8}
+
+
+def checkSeq(seq: str, legalBase=['A', 'T', 'C', 'G']):
+    for base in seq.upper():
+        if base not in legalBase:
+            print(f"[checkSeq] Warnning:: illegal base {base}")
+            return False
+    return True
+
+
+def numSequence(in_listSequence: list, in_dictNumTable: dict = G_DICT_NUM_DNA_TABLE,
+                in_intLength=2048):
+    numberDNA = []
+    for dna in in_listSequence:
+        currentSeq = []
+        for alphabet in dna:
+            if alphabet not in in_dictNumTable:
+                print(f"Error :: can't find current vocabulary {alphabet}")
+                break
+            else:
+                currentSeq.append(in_dictNumTable[alphabet])
+        if len(dna) == len(currentSeq):
+            while len(currentSeq) < in_intLength:
+                currentSeq.append(in_dictNumTable['Appending'])
+            numberDNA.append(currentSeq)
+        else:
+            print(f"Loss sequence {dna}")
+    return numberDNA
+
+
+def shuffleList(seq: list, label: list):
+    combinedDataset = list(zip(seq, label))
+    random.shuffle(combinedDataset)
+    seq, label = zip(*combinedDataset)
+
+    return list(seq), list(label)
+
+
+def readSGRN(in_strFile: Path, columns=['LogZ_Log_delta_1', 'LogZ_Log_delta_2', 'LogZ_Log_delta_3', 'LogZ_Log_delta_4']):
+    data = pd.read_excel(in_strFile, sheet_name='crisp')
+    resultTrain = {'sequence': [], 'label': []}
+    resultTest = {'sequence': [], 'label': []}
+    promoter = data['Promoter']
+    sgRNA = data['sgRNA']
+    pam = data['PAM']
+    label1 = data[columns[0]]
+    label2 = data[columns[1]]
+    label3 = data[columns[2]]
+    label4 = data[columns[3]]
+
+    promoterSeq = ''
+    testFlag = False
+    for index in range(len(promoter)):
+        if str(promoter[index]).strip().startswith('LongPromoter') or 'None' == str(promoter[index]).strip():    # First line
+            continue
+        if '无' == sgRNA[index]:
+            promoterSeq = promoter[index]
+            testFlag = True
+            continue
+        else:
+            if not checkSeq(promoterSeq):
+                print(f"Illegal Promoter: {promoterSeq}")
+            if isinstance(sgRNA[index], (int, float)) or not checkSeq(str(sgRNA[index].strip())):
+                print(f"Illegal sgRNA: {sgRNA[index]}")
+            if isinstance(pam[index], (int, float)) or not checkSeq(str(pam[index].strip())):
+                print(f"Illegal PAM: {pam[index]}")
+            temSeq = promoterSeq.strip().upper() + '+' + sgRNA[index].strip().upper() + '+' + pam[index].strip().upper()    # with special token
+            print(f"Label : {label1[index], label2[index], label3[index], label4[index]}")
+            if not pd.isna(label1[index]) and not pd.isna(label2[index]) and\
+                not pd.isna(label3[index]) and not pd.isna(label4[index]):
+                if testFlag:
+                    resultTest['sequence'].append(temSeq.upper())
+                    lab = [label1[index], label2[index], label3[index], label4[index]]
+                    random.shuffle(lab)
+                    resultTest['label'].append(lab[0])
+                    testFlag = False
+                else:
+                    resultTrain['sequence'].append(temSeq.upper())
+                    resultTrain['label'].append([label1[index], label2[index], label3[index], label4[index]])
+
+    return resultTrain, resultTest
+
+def readSGRNForPrediction(in_strFile: Path):
+    data = pd.read_excel(in_strFile, sheet_name='crisp')
+    resultDataSet = []
+    promoter = data['Promoter']
+    sgRNA = data['sgRNA']
+    pam = data['PAM']
+    promoterSeq = ''
+    testFlag = False
+    for index in range(len(promoter)):
+        if str(promoter[index]).strip().startswith('LongPromoter') or 'None' == str(promoter[index]).strip():    # First line
+            continue
+        if '无' == sgRNA[index]:
+            promoterSeq = promoter[index]
+            testFlag = True
+            continue
+        else:
+            if not checkSeq(promoterSeq):
+                print(f"Illegal Promoter: {promoterSeq}")
+            if isinstance(sgRNA[index], (int, float)) or not checkSeq(str(sgRNA[index].strip())):
+                print(f"Illegal sgRNA: {sgRNA[index]}")
+            if isinstance(pam[index], (int, float)) or not checkSeq(str(pam[index].strip())):
+                print(f"Illegal PAM: {pam[index]}")
+            temSeq = promoterSeq.strip().upper() + '+' + sgRNA[index].strip().upper() + '+' + pam[index].strip().upper()    # with special token
+            if testFlag:
+                resultDataSet.append(temSeq.upper())
+
+
+    return resultDataSet
+
+
+def mainLB(dataFile: Path, outFolder: Path):
+    tpmColumns = ['Lablel-1', 'Lablel-2', 'Lablel-3', 'Lablel-4']
+    resTrain, resTest = readSGRN(dataFile, tpmColumns)
+    print(f'Training set size is {len(resTrain)}, Test set size is {len(resTest)}')
+    t_strOutputFolder = outFolder
+    # training dataset
+    numsequence = numSequence(resTrain["sequence"], G_DICT_NUM_DNA_TABLE, 2048)
+    if len(numsequence) != len(resTrain['label']):
+        raise Exception("Loss some sequence after numbering dna")
+    # shuffle training dataset
+    combinedDataset = list(zip(numsequence, resTrain['label']))
+    random.shuffle(combinedDataset)
+    numsequence, resTrain['label'] = zip(*combinedDataset)
+    numsequence = list(numsequence)
+    resTrain['label'] = list(resTrain['label'])
+    # extract training dataset
+    trainingSet = []
+    trainingLab = []
+    for index in range(len(numsequence)):
+        for lab in resTrain['label'][index]:
+            trainingSet.append(numsequence[index])
+            trainingLab.append(lab)
+    trainingSet, trainingLab = shuffleList(trainingSet, trainingLab)
+    # test dataset
+    numsequence = numSequence(resTest["sequence"], G_DICT_NUM_DNA_TABLE, 2048)
+    if len(numsequence) != len(resTest['label']):
+        raise Exception("Loss some sequence after numbering dna")
+    testSet = []
+    testLab = []
+    for index in range(len(numsequence)):
+        testSet.append(numsequence[index])
+        testLab.append(resTest["label"][index])
+    testSet, testLab = shuffleList(testSet, testLab)
+    t_strOutputFile = os.path.join(t_strOutputFolder, 'training_gene.jsonl')
+    with open(t_strOutputFile, 'w') as out:
+        for seq in trainingSet:
+            out.write(json.dumps(seq) + '\n')
+    t_strOutputFile = os.path.join(t_strOutputFolder, 'trainingLab_gene.json')
+    with open(t_strOutputFile, 'w') as out:
+        out.write(json.dumps(trainingLab) + '\n')
+    t_strOutputFile = os.path.join(t_strOutputFolder, 'test_gene.jsonl')
+    with open(t_strOutputFile, 'w') as out:
+        for seq in testSet:
+            out.write(json.dumps(seq) + '\n')
+    t_strOutputFile = os.path.join(t_strOutputFolder, 'testLab_gene.json')
+    with open(t_strOutputFile, 'w') as out:
+        out.write(json.dumps(testLab) + '\n')
+
+def mainForPrediction(dataFile: Path, outFolder: Path):
+    sequences = readSGRNForPrediction(dataFile)
+    print(f'Data set size is {len(sequences)}')
+    t_strOutputFolder = outFolder
+    # numerate dataset
+    numsequence = numSequence(sequences, G_DICT_NUM_DNA_TABLE, 2048)
+    t_strOutputFile = os.path.join(t_strOutputFolder, 'predict_gene.jsonl')
+    with open(t_strOutputFile, 'w') as out:
+        for seq in numsequence:
+            out.write(json.dumps(seq) + '\n')
+
+
+if __name__ == '__main__':
+    t_dateTime = datetime.datetime.now()
+    print("Begin time : %s" % t_dateTime)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataFile", default='data/crispData.xlsx', type=str, required=True,
+        help="The relative or absolute path of the crisp data file that needs to be tokenized"
+    )
+    parser.add_argument(
+        "--output_dir", default='input', type=str, help="Folder for storing output files."
+    )
+    parser.add_argument(
+        "--prediction", default=0, type=int,
+        help="A parameter value of 0 means that the tokenize labeled dataset is prepared for training the model, "
+             "while a parameter value of 1 means the tokenize dataset to be predicted."
+    )
+    args = parser.parse_args()
+
+    if 0 == args.prediction:
+        mainLB(args.dataFile, args.output_dir)
+    elif 1 == args.prediction:
+        mainForPrediction(args.dataFile, args.output_dir)
+    else:
+        raise Exception("The prediction parameter is incorrect. Please check and try again.")
+
+    t_dateTime = datetime.datetime.now()
+    print("End time : %s" % t_dateTime)
